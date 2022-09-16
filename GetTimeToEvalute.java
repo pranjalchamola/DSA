c16076fcf2532547ed30155ced4bdcfa5729a39d
pranjalchamola <pranjalchamola@gmail.com>
2022-09-16 15:53:51 +0530

    ..

---
+++ GetTimeToEvalute.java
@@ -0,0 +1,17 @@
+package GitCode;
+
+public class GetTimeToEvalute {
+    long startTime=0;
+    long endtime=0;
+
+    public void start()
+    {
+        startTime=System.currentTimeMillis();
+    }
+
+    public void end()
+    {
+        endtime=System.currentTimeMillis();
+        System.out.println(endtime-startTime);
+    }
+}
